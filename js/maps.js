3c2f650db75b7f27a2fd9f9891e0d790222d00f1
BankaSTR <prokonov3@mail.ru>
2022-06-24 18:09:25 +0400

    intel55555

---
+++ js/maps.js
@@ -0,0 +1,8 @@
+function test5() {
+    test = document.querySelector(".maps1")
+    test.style.display="block"
+
+    if(test.style.display="block"){
+        test.style.display="none"
+    }
+}
---
+++ shops/js/maps.js
@@ -0,0 +1,45 @@
+function maps1() {
+    maps1 = document.querySelector(".maps1")
+    if(maps1.style.display == "block"){
+        maps1.style.display="none"
+    }else{
+        maps1.style.display="block"
+    }
+}
+
+function maps2() {
+    maps2 = document.querySelector(".maps15")
+    if(maps2.style.display == "block"){
+        maps2.style.display="none"
+    }else{
+        maps2.style.display="block"
+    }
+}
+
+function maps3() {
+    maps3 = document.querySelector(".maps155")
+    if(maps3.style.display == "block"){
+        maps3.style.display="none"
+    }else{
+        maps3.style.display="block"
+    }
+}
+
+function maps4() {
+    maps4 = document.querySelector(".maps55")
+    if(maps4.style.display == "block"){
+        maps4.style.display="none"
+    }else{
+        maps4.style.display="block"
+    }
+}
+
+function maps5() {
+    maps5 = document.querySelector(".maps159")
+    if(maps5.style.display == "block"){
+        maps53.style.display="none"
+    }else{
+        maps5.style.display="block"
+    }
+}
+
